9ae3282f57eac89ee937d3f5bbdda24fe5c4c005
Angeline Mulet-Marquis <angeline.muletmarquis@smarkets.com>
2017-12-13 11:44:15 +0000

    070 Climbing stairs

---
+++ 070-climbing_stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ways = {
+            1: 1,
+            2: 2,
+        }
+        for step in range(3, n + 1):
+            ways[step] = ways[step-1] + ways[step-2]
+
+        return ways[n]
